324bd2b274588f6111e4c8ac567365067977e69f
suporteticopimaq <125459793+suporteticopimaq@users.noreply.github.com>
2023-02-22 10:41:16 -0300

    Atualizações...

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+# Altera a senha do usuario root
 echo "root:$ROOT_PASSWORD" | chpasswd
+
+# Altera o diretorio primario que aparecerá quando realizar o login
 echo "cd /var/www/html/" >> /root/.bashrc
 
+# Inicia os serviços Web e SSH
 service ssh start
 service apache2 start
 
+# Deixa o container rodando infinitamente
 sleep infinity
